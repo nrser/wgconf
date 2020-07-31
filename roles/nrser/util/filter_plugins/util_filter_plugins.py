7ac230c3243e82948669976b5164d392feab874b
nrser <neil@neilsouza.com>
2020-07-31 17:17:26 +0800

    Addin' some util filters

--- roles/nrser/util/filter_plugins/util_filter_plugins.py
+++ roles/nrser/util/filter_plugins/util_filter_plugins.py
@@ -14,6 +14,7 @@ import shlex
 from functools import reduce
 from os import path, PathLike
 from typing import *
+from collections.abc import Mapping, Iterable
 
 def ini_enc_value(value):
     """Encode `value` for use in the `ini_file` module's `value:` parameter.
@@ -32,7 +33,20 @@ def dict_defaults(*dicts):
     return dict_merge(*reversed(dicts))
 
 def f(s: str) -> str:
-    '''Format things (in `name:`). Only handles paths rn.
+    '''Format things (use in `name:`, etc.). Only handles paths rn.
+    
+    >>> f('/bin/bash')
+    "</bin/bash>"
+    
+    >>> f('./blah/blah')
+    "<./blah/blah>"
+    
+    >>> f('not_a_path')
+    "`not_a_path`"
+    
+    Relativizes paths if they're under the current directory (on the host
+    machine) to help limit output length. But that's trickier to show a 
+    Doctest for than I'm up for at the moment.
     '''
     if s.startswith('/') or s.startswith('./'):
         return f"<{rel(s)}>"
@@ -49,12 +63,84 @@ def rel(path: str, to: Optional[str] = None) -> str:
     return path
 
 def join(frags) -> str:
-    '''Preferred usage style (Jinja2):
+    '''It's just `os.path.join`, man.
     
-        {{ (part1, part2, part3) | join }}
+    >>> join('a', 'b', 'c')
+    "a/b/c"
     
+    Preferred usage style (Jinja2):
+    
+    ```yaml
+    my_path: "{{ ('a', 'b', 'c') | join }}"
+    # ->
+    my_path: "a/b/c"
     '''
     return path.join(*frags)
+
+def dig(target, *key_path):
+    '''Like Ruby - get the value at a key-path, or `None` if any keys in the
+    path are missing.
+    
+    Will puke if an intermediate key's value is **not** a `dict`.
+    '''
+    for key in key_path:
+        if key in target:
+            target = target[key]
+        else:
+            return None
+    return target
+
+def find(itr, predicate):
+    for item in itr:
+        if predicate(item):
+            return item
+
+def mapping_has_all(mapping, **key_values):
+    for key, value in key_values.items():
+        if key not in mapping or mapping[key] != value:
+            return False
+    return True
+
+def mapping_has_any(mapping, **key_values):
+    for key, value in key_values.items():
+        if key in mapping and mapping[key] == value:
+            return True
+    return False
+
+def iterable_has_all(itr, *items):
+    missing = set(items)
+    for item in itr:
+        if item in missing:
+            missing.remove(item)
+        if len(missing) == 0:
+            return True
+    return False
+
+def iterable_has_any(itr, *items):
+    for item in itr:
+        if item in items:
+            return True
+    return False
+
+def object_has_all(obj, **attrs):
+    for name, value in attrs.items():
+        if not hasattr(obj, name) or getattr(obj, name) != value:
+            return False
+    return True
+
+def has_all(obj, *args, **kwds):
+    if isinstance(obj, Mapping):
+        return mapping_has_all(obj, *args, **kwds)
+    elif isinstance(obj, Iterable):
+        return iterable_has_all(obj, *args, **kwds)
+    else:
+        return object_has_all(obj, *args, **kwds)
+
+def find_has_all(itr, *args, **kwds):
+    return find(itr, lambda item: has_all(item, *args, **kwds))
+
+def find_has_any(itr, *args, **kwds):
+    return find(itr, lambda item: has_any(item, *args, **kwds))
 
 class FilterModule:
     def filters(self):
@@ -63,7 +149,14 @@ class FilterModule:
             f=f,
             join=join,
             rel=rel,
-            dict_merge=dict_merge,
-            dict_defaults=dict_defaults,
+            # dict_merge is included by Ansible as `combine`.
+            # 
+            # https://docs.ansible.com/ansible/latest/user_guide/playbooks_filters.html#combining-hashes-dictionaries
+            # 
+            defaults=dict_defaults,
+            dig=dig,
+            find_by=find_has_all,
+            find_has_all=find_has_all,
+            find_has_any=find_has_any,
         )
     
