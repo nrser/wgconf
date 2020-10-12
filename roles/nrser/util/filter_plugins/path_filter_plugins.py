d6513fffd2c65e6acc5a31ba8b7f163100951c7e
nrser <neil@neilsouza.com>
2020-10-12 20:33:47 +0800

    Add os-switching to to_path filter

--- roles/nrser/util/filter_plugins/path_filter_plugins.py
+++ roles/nrser/util/filter_plugins/path_filter_plugins.py
@@ -1,10 +1,29 @@
-from pathlib import Path
+from pathlib import PurePosixPath, PureWindowsPath
 from os import path
 from typing import *
 
 import nansi.utils.path
+from nansi.utils.collections import flatten
+from nansi.os_resolve import os_map_resolve
 
-def to_path(frags) -> str:
+def to_posix_path(path_segments) -> str:
+    return str(PurePosixPath(*flatten(path_segments)))
+
+def to_win_path(path_segments) -> str:
+    return str(PureWindowsPath(*flatten(path_segments)))
+
+def to_os_path(path_segments, facts) -> str:
+    return os_map_resolve(
+        facts,
+        {
+            'family': {
+                'windows': to_win_path,
+            },
+            'any': to_path,
+        }
+    )(path_segments)
+
+def to_path(path_segments, facts=None) -> str:
     '''It's just `os.path.join`, man.
     
     >>> to_path(('a', 'b', 'c'))
@@ -15,13 +34,16 @@ def to_path(frags) -> str:
     >>> template("{{ ('a', 'b', 'c') | to_path }}")
     'a/b/c'
     '''
-    return path.join(*frags)
+    if facts is None:
+        return to_posix_path(path_segments)
+    return to_os_path(path_segments, facts=facts)
 
 class FilterModule:
     def filters(self):
         return dict(
             to_path         = to_path,
               path          = to_path,  # Old name
+            to_win_path     = to_win_path,
             rel             = nansi.utils.path.rel,
         )
 
