2b941aa8d8f23769f0f26f8c277e8cf27bb2d84b
nrser <neil@neilsouza.com>
2020-12-23 19:18:47 +0800

    Lint

--- packages/nansi/nansi/logging/log_record.py
+++ packages/nansi/nansi/logging/log_record.py
@@ -1,8 +1,8 @@
 import logging
 
-class LogRecord(logging.LogRecord):    
+class LogRecord(logging.LogRecord):
     def getMessage(self) -> str:
         try:
             return super().getMessage()
-        except TypeError as error:
+        except TypeError:
             return str(self.msg)
