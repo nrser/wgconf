78690f353c1dde02874c1e3f42a29d9df53d188f
nrser <neil@neilsouza.com>
2020-12-23 19:24:06 +0800

    Fix connect utility and use in to_url filter

--- packages/nansi/nansi/utils/strings.py
+++ packages/nansi/nansi/utils/strings.py
@@ -1,7 +1,12 @@
-from nansi.utils.collections import flat_map
+import urllib.parse
+
+from nansi.utils.collections import flat_map, iter_flat
+
 
 def connect(*parts, seperator: str = "/") -> str:
-    '''
+    """
+    A generalized version of `os.path.join` that handles nested lists.
+
     >>> connect(
     ...     ('/etc', 'dir'),
     ...     'sir',
@@ -9,15 +14,44 @@ def connect(*parts, seperator: str = "/") -> str:
     ...     'file.ext',
     ... )
     '/etc/dir/sir/projects/objects/file.ext'
-    '''
-    return seperator.join((
-        part for
-        index, part in
-        enumerate(flat_map(lambda part: str(part).split(seperator), parts))
-        if len(part) > 0 or index == 0
-    ))
-    
-if __name__ == '__main__':
+
+    >>> connect(
+    ...     ('/etc/', '/dir'),
+    ...     'sir',
+    ...     ('/projects', 'objects/'),
+    ...     'file.ext',
+    ... )
+    '/etc/dir/sir/projects/objects/file.ext'
+
+    >>> connect('http://example.com', 'c')
+    'http://example.com/c'
+
+    >>> connect('a', '//', 'c')
+    'a/c'
+
+    >>> connect('/', 'a', 'b', 'c')
+    '/a/b/c'
+    """
+    return seperator.join(
+        (
+            part
+            for index, part in (
+                (
+                    index,
+                    (
+                        part.rstrip(seperator)
+                        if index == 0
+                        else part.strip(seperator)
+                    ),
+                )
+                for index, part in enumerate(iter_flat(parts))
+            )
+            if index == 0 or part != ""
+        )
+    )
+
+
+if __name__ == "__main__":
     import doctest
-    doctest.testmod()  
-    
+
+    doctest.testmod()
--- roles/nrser/util/filter_plugins/url_filter_plugins.py
+++ roles/nrser/util/filter_plugins/url_filter_plugins.py
@@ -1,39 +1,32 @@
 from typing import *
 import urllib.parse
-import os
-import os.path
 
-from nansi.utils.collections import flatten
 from nansi.utils.strings import connect
-    
+
 def to_url(args,  allow_fragments=True) -> str:
     '''It's just `urllib.parse.urljoin()`. Don't use `join()`, it gets weird if
     any segments start with a `/`.
-    
+
     >>> to_url(('http://example.com', '/a/b/c'))
     'http://example.com/a/b/c'
-    
+
     Preferred usage style (Jinja2):
-    
+
     >>> template("{{ ('http://example.com', '/a/b/c') | to_url }}")
     'http://example.com/a/b/c'
-    
+
     >>> to_url(('https://some.example.com', 'projects', 'blah.tar.gz'))
     'https://some.example.com/projects/blah.tar.gz'
-    
+
     >>> template("""{{
     ...     ('https://some.example.com', '/projects/', '/blah.tar.gz')
     ...     | to_url
     ... }}""")
     'https://some.example.com/projects/blah.tar.gz'
-    
+
     '''
-    base, *path = args
-    return urllib.parse.urljoin(
-        base,
-        connect(path),
-        allow_fragments=allow_fragments,
-    )
+    # return url_join(*args, allow_fragments=allow_fragments)
+    return connect(*args)
 
 class FilterModule:
     def filters(self):
