c36517291394605d2e58f6703399b73a5c9bb5a4
nrser <neil@neilsouza.com>
2021-01-15 09:16:33 +0800

    Get rid of file

--- packages/nansi/nansi/logging/log_record.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-class LogRecord(logging.LogRecord):
-    def getMessage(self) -> str:
-        try:
-            return super().getMessage()
-        except TypeError:
-            return str(self.msg)
